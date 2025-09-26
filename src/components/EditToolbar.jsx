a2b0c9bb81215424d3556e47b125aa6b97426d14
Matthew Daos <143854012+matthewdaos@users.noreply.github.com>
2025-09-26 10:18:46 -0400

    Added disable fuunction for the buttons in the toolbar

--- src/components/EditToolbar.jsx
+++ src/components/EditToolbar.jsx
@@ -18,6 +18,7 @@ export default class EditToolbar extends React.Component {
                 type="button" 
                 id='add-song-button' 
                 value="+" 
+                disabled={!canAddSong}
                 className={addSongClass}
                 onClick={addSongCallback}
             />
@@ -25,6 +26,7 @@ export default class EditToolbar extends React.Component {
                 type="button" 
                 id='undo-button' 
                 value="⟲" 
+                disabled={!canUndo}
                 className={undoClass} 
                 onClick={undoCallback}
             />
@@ -32,6 +34,7 @@ export default class EditToolbar extends React.Component {
                 type="button" 
                 id='redo-button' 
                 value="⟳" 
+                disabled={!canRedo}
                 className={redoClass} 
                 onClick={redoCallback}
             />
@@ -39,6 +42,7 @@ export default class EditToolbar extends React.Component {
                 type="button" 
                 id='close-button' 
                 value="&#x2715;" 
+                disabled={!canClose}
                 className={closeClass} 
                 onClick={closeCallback}
             />
