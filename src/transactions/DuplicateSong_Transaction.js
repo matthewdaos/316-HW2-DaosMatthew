ce643e33d3ad0c02d0270053a4c091af362f0111
Matthew Daos <143854012+matthewdaos@users.noreply.github.com>
2025-09-26 14:28:20 -0400

    Changed AddSong to DuplicateSong

--- src/transactions/DuplicateSong_Transaction.js
+++ src/transactions/DuplicateSong_Transaction.js
@@ -1,6 +1,6 @@
 import { jsTPS_Transaction } from "jstps";
 /**
- * AddSong_Transaction
+ * DuplicateSong_Transaction
  * 
  * This class represents a transaction that works with drag
  * and drop. It will be managed by the transaction stack.
